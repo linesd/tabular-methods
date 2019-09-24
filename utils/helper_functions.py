9d0bb6c36a46b6670e95f751cffea3b9524c3f36
linesd <david.lines.dl@gmail.com>
2019-09-24 15:58:11 +0100

    update plots and helper functions

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -8,3 +8,47 @@ def seq_to_col_row(seq, num_cols):
     r = floor(seq / num_cols)
     c = seq - r * num_cols
     return np.array([[r, c]])
+
+def create_policy_direction_arrays(model, policy):
+    """
+     define the policy directions
+     0 - up    [0, 1]
+     1 - down  [0, -1]
+     2 - left  [-1, 0]
+     3 - right [1, 0]
+    :param policy:
+    :return:
+    """
+    # action options
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+    # intitialize direction arrays
+    U = np.zeros((model.num_rows, model.num_cols))
+    V = np.zeros((model.num_rows, model.num_cols))
+
+    for state in range(model.num_states-1):
+        # get index of the state
+        i = tuple(seq_to_col_row(state, model.num_cols)[0])
+        # define the arrow direction
+        if policy[state] == UP:
+            U[i] = 0
+            V[i] = 0.5
+        elif policy[state] == DOWN:
+            U[i] = 0
+            V[i] = -0.5
+        elif policy[state] == LEFT:
+            U[i] = -0.5
+            V[i] = 0
+        elif policy[state] == RIGHT:
+            U[i] = 0.5
+            V[i] = 0
+
+    return U, V
+
+
+
+
+
--- utils/plots.py
+++ utils/plots.py
@@ -1,6 +1,43 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from utils.helper_functions import create_policy_direction_arrays
+import numpy as np
 
-def plot_gridworld(model):
+def plot_gridworld(model, value_function, policy):
 
-    plt.figure()
-    plt.imshow(model.)
+    fig, ax = plt.subplots(1)
+    # colobar max and min
+    vmin = np.min(value_function)
+    vmax = np.max(value_function)
+    # reshape and set obstructed states to low value
+    val = value_function[:-1,0].reshape(model.num_rows,model.num_cols)
+    index = model.obs_states
+    val[index[:, 0], index[:, 1]] = -100
+    plt.imshow(val, vmin=vmin, vmax=vmax)
+    plt.colorbar(label="Value function")
+    # create start and end patches
+    start = patches.Circle(tuple(np.flip(model.start_state[0])), 0.2,linewidth=1,
+                           edgecolor='g', facecolor='g', label="Start")
+    end = patches.RegularPolygon(tuple(np.flip(model.goal_state[0])), numVertices=5,
+                                 radius=0.25, orientation=np.pi, edgecolor='r',
+                                 facecolor='r',label="Goal")
+    for i in range(model.obs_states.shape[0]):
+        obstructed = patches.Rectangle(tuple(np.flip(model.obs_states[i,:])-0.35), 0.7, 0.7,
+                                      linewidth=1, edgecolor='orange', facecolor='orange',
+                                       label="Obstructed" if i == 0 else None)
+        ax.add_patch(obstructed)
+    ax.add_patch(start)
+    ax.add_patch(end)
+    # define the gridworld
+    X = np.arange(0, model.num_cols, 1)
+    Y = np.arange(0, model.num_rows, 1)
+    # define the policy direction arrows
+    U, V = create_policy_direction_arrays(model, policy)
+    # remove the obstructions and final state arrows
+    ra = np.vstack((model.obs_states, model.goal_state))
+    U[ra[:,0],ra[:,1]] = np.nan
+    V[ra[:,0],ra[:,1]] = np.nan
+    plt.quiver(X, Y, U, V, label="Policy")
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.12),
+               fancybox=True, shadow=True, ncol=4)
+    plt.show()
