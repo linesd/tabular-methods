79fa62db098291e5b8050860a95bc4803dbadb0a
linesd <david.lines.dl@gmail.com>
2019-09-25 01:05:35 +0100

    update plot docstring

--- utils/plots.py
+++ utils/plots.py
@@ -3,18 +3,49 @@ import matplotlib.patches as patches
 from utils.helper_functions import create_policy_direction_arrays
 import numpy as np
 
-def plot_gridworld(model, value_function, policy, path=None):
+def plot_gridworld(model, value_function=None, policy=None, title=None, path=None):
+    """
+    Plots the grid world solution.
 
+    Parameters
+    ----------
+    model : python object
+        Holds information about the environment to solve
+        such as the reward structure and the transition dynamics.
+
+    value_function : numpy array of shape (N, 1)
+        Value function of the environment where N is the number
+        of states in the environment.
+
+    policy : numpy array of shape (N, 1)
+        Optimal policy of the environment.
+
+    title : string
+        Title of the plot. Defaults to None.
+
+    path : string
+        Path to save image. Defaults to None.
+    """
     fig, ax = plt.subplots()
     # colobar max and min
     vmin = np.min(value_function)
     vmax = np.max(value_function)
-    # reshape and set obstructed states to low value
-    val = value_function[:-1,0].reshape(model.num_rows,model.num_cols)
-    index = model.obs_states
-    val[index[:, 0], index[:, 1]] = -100
-    plt.imshow(val, vmin=vmin, vmax=vmax)
-    plt.colorbar(label="Value function")
+
+    if value_function is not None:
+        # reshape and set obstructed states to low value
+        val = value_function[:-1, 0].reshape(model.num_rows, model.num_cols)
+        index = model.obs_states
+        val[index[:, 0], index[:, 1]] = -100
+        plt.imshow(val, vmin=vmin, vmax=vmax)
+        plt.colorbar(label="Value function")
+    else:
+        val = np.zeros((model.num_rows, model.num_rows))
+        plt.imshow(val, vmin=vmin, vmax=vmax)
+        plt.xticks(np.arange(-0.5, model.num_rows+0.5, step=1))
+        plt.yticks(np.arange(-0.5, model.num_cols+0.5, step=1))
+        plt.grid(which="major")
+        plt.colorbar(label="Value function")
+
     # create start and end patches
     start = patches.Circle(tuple(np.flip(model.start_state[0])), 0.2,linewidth=1,
                            edgecolor='b', facecolor='b', label="Start")
@@ -35,20 +66,24 @@ def plot_gridworld(model, value_function, policy, path=None):
     for i in range(model.bad_states.shape[0]):
         bad = patches.Wedge(tuple(np.flip(model.bad_states[i,:])), 0.3, 20, -20,
                             linewidth=1, edgecolor='r', facecolor='none',
-                            label="Bad" if i == 0 else None)
+                            label="Bad state" if i == 0 else None)
         ax.add_patch(bad)
     # define the gridworld
     X = np.arange(0, model.num_cols, 1)
     Y = np.arange(0, model.num_rows, 1)
-    # define the policy direction arrows
-    U, V = create_policy_direction_arrays(model, policy)
-    # remove the obstructions and final state arrows
-    ra = np.vstack((model.obs_states, model.goal_states))
-    U[ra[:,0],ra[:,1]] = np.nan
-    V[ra[:,0],ra[:,1]] = np.nan
-    plt.quiver(X, Y, U, V, label="Policy")
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.22),
+
+    if policy is not None:
+        # define the policy direction arrows
+        U, V = create_policy_direction_arrays(model, policy)
+        # remove the obstructions and final state arrows
+        ra = np.vstack((model.obs_states, model.goal_states))
+        U[ra[:,0],ra[:,1]] = np.nan
+        V[ra[:,0],ra[:,1]] = np.nan
+        plt.quiver(X, Y, U, V, label="Policy")
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
                fancybox=True, shadow=True, ncol=3)
+    if title is not None:
+        plt.title(title, fontdict=None, loc='center')
     if path is not None:
         plt.savefig(path, dpi=300)
     plt.show()
