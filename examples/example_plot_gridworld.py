e119787b544e29c253fb7efea8d2249b5409f503
linesd <david.lines.dl@gmail.com>
2019-09-30 19:04:16 +0100

    Update examples

--- examples/example_plot_gridworld.py
+++ examples/example_plot_gridworld.py
@@ -11,14 +11,14 @@ from utils.plots import plot_gridworld
 # specify world parameters
 num_cols = 10
 num_rows = 10
-obstructions = np.array([[0, 7], [1, 1], [1, 2], [1, 3], [1, 7], [2, 1], [2, 3],
-                         [2, 7], [3, 1], [3, 3], [3, 5], [3, 7], [4, 3], [4, 5],
-                         [5, 3], [5, 9], [6, 3], [6, 9], [7, 1], [7, 6], [7, 7],
-                         [7, 8], [7, 9], [8, 1], [8, 5], [8, 6], [9, 1]])
-bad_states = np.array([[1, 9], [4, 2], [4, 4], [7, 5], [8, 2], [9, 9]])
-restart_states = np.array([[5, 4], [6, 8], [8, 8]])
-start_state = np.array([[0, 4]])
-goal_states = np.array([[0, 9], [2, 2], [8, 7]])
+obstructions = np.array([[0,7],[1,1],[1,2],[1,3],[1,7],[2,1],[2,3],
+                         [2,7],[3,1],[3,3],[3,5],[4,3],[4,5],[4,7],
+                         [5,3],[5,7],[5,9],[6,3],[6,9],[7,1],[7,6],
+                         [7,7],[7,8],[7,9],[8,1],[8,5],[8,6],[9,1]])
+bad_states = np.array([[1,9],[4,2],[4,4],[7,5],[9,9]])
+restart_states = np.array([[3,7],[8,2]])
+start_state = np.array([[0,4]])
+goal_states = np.array([[0,9],[2,2],[8,7]])
 
 # create model
 gw = GridWorld(num_rows=num_rows,
@@ -31,7 +31,7 @@ gw.add_obstructions(obstructed_states=obstructions,
 gw.add_rewards(step_reward=-1,
                goal_reward=10,
                bad_state_reward=-6,
-               restart_state_reward=-100)
+               restart_state_reward=-10)
 gw.add_transition_probability(p_good_transition=0.7,
                               bias=0.5)
 gw.add_discount(discount=0.9)
